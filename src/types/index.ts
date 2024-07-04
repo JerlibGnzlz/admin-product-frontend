e4f86dfb1b7c4a140202705fef21e4847aee0c17
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-07-03 21:09:05 -0300

    deploy

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,30 +1,43 @@
-// import { boolean, number, object, string, array, InferTupleOutput } from 'valibot';
-import { v } from 'valibot';
+import { boolean, number, object, string, array, InferTupleOutput } from 'valibot';
 
-import { z } from 'zod';
+// import { z } from 'zod';
 
-export const DraftProductSchema = v.object({
-    name: v.string(),
-    price: v.number()
+export const DraftProductSchema = object({
+    name: string(),
+    price: number()
 })
 
-export const ProductSchema = v.pipe({
-    id: v.number(),
-    name: v.string(),
-    price: v.number(),
-    active: v.boolean(),
+export const ProductSchema = object({
+    id: number(),
+    name: string(),
+    price: number(),
+    active: boolean(),
 })
 
-export const ProductSchemaZod = z.object({
-    id: z.number(),
-    name: z.string(),
-    price: z.number(),
-    active: z.boolean()
-})
+// export const ProductSchemaZod = z.object({
+//     id: z.number(),
+//     name: z.string(),
+//     price: z.number(),
+//     active: z.boolean()
+// })
+
+import { z, ZodSchema } from 'zod';
+
+export const ProductSchemaZod: ZodSchema<{
+    id?: number;
+    name?: string;
+    price?: number;
+    active?: boolean;
+}> = z.object({
+    id: z.number().optional(),
+    name: z.string().optional(),
+    price: z.number().optional(),
+    active: z.boolean().optional(),
+});
 
 
-export type Product = v.InferTupleOutput<typeof ProductSchema>
+export type Product = InferTupleOutput<typeof ProductSchema>
 
-export const ProductsSchema = v.array(ProductSchema)
+export const ProductsSchema = array(ProductSchema)
 
 
